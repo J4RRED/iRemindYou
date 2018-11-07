c13dc4a572e6e9c16d3d4a33efd064ca27dd3c05
J4RRED <desalmejarred@gmail.com>
2018-11-07 10:25:16 -0600

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addReminder, deleteReminder, clearReminders } from '../actions';
+import moment from 'moment';
+import Roll from 'react-reveal/Roll';
+import ScrollableAnchor from 'react-scrollable-anchor';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: " ",
+      dueDate: ''
+    }
+  }
+
+  addReminder() {
+    console.log('this.state.dueDate', this.state.dueDate);
+    this.props.addReminder(this.state.text, this.state.dueDate);
+  }
+
+  deleteReminder(id) {
+    this.props.deleteReminder(id);
+  }
+
+  renderReminders() {
+    const { reminders } = this.props;
+    return (
+    <ul className='list-group col-sm-4'>
+      {
+        reminders.map(reminder => {
+          return (
+            <li key={reminder.id} className='list-group-item'>
+              <div className='list-item'>
+                <div>{reminder.text}</div>
+                <div><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div>
+              </div>
+              <div
+                className='list-item delete-button'
+                onClick={() => this.deleteReminder(reminder.id)}
+                >
+                &#x2715;
+              </div>
+            </li>
+          )
+        })
+      }
+    </ul>)
+  }
+
+  render() {
+    return (
+      /* jsx for title page only */
+      <div className='App'>
+        <section className='top-page'>
+          <div
+            className='roller center-div center-all'
+            >
+            <Roll top cascade>
+              <a href='bottom-page'>
+                iRemindYou
+              </a>
+            </Roll>
+          </div>
+        </section>
+
+        {/* Actual jsx for reminder app */}
+        <section className='bottom-page'>
+        <div
+          className='title'>
+          iRemindYou
+        </div>
+        <div className='form-inline reminder-form'>
+          <div className='form-group'>
+            <input
+              className='form-control'
+              placeholder='I have to...'
+              onChange={event => this.setState({text: event.target.value})}
+            />
+            <input
+              className='form-control'
+              type='datetime-local'
+              onChange={event => this.setState({dueDate: event.target.value})}
+              />
+          </div>
+          <button
+            type='button'
+            className='btn btn-success button'
+            onClick={() => this.addReminder()}
+          >
+            Add Reminder
+          </button>
+        </div>
+        {/* return reminders list */}
+        { this.renderReminders() }
+        {/* clear all button */}
+        <div
+          className='btn btn-danger clear-button'
+          onClick={() => this.props.clearReminders()}
+          >
+            Clear Reminders
+        </div>
+      </section>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    reminders: state
+  }
+}
+
+export default connect(mapStateToProps, { addReminder, deleteReminder, clearReminders })(App);
+
+// all you need is a constant, action w action creator and a reducer; all to store one big state object
